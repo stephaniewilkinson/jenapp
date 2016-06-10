15f81fa9f2182c2801333bce38c15c9cca4d5b11
Stephanie Wilkinson <what.happens@gmail.com>
2016-06-10 11:05:55 -0700

    devise working

--- db/migrate/20160610045842_create_clients.rb
+++ db/migrate/20160610045842_create_clients.rb
@@ -1,6 +1,7 @@
 class CreateClients < ActiveRecord::Migration
   def change
     create_table :clients do |t|
+      t.integer :user_id
       t.string :name
       t.string :phone
       t.string :email
